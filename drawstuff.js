279d9927d69af07383f083f53227fb0433197589
Jainpriyal <jainpriyal168@gmail.com>
2017-08-22 12:58:29 -0700

    modifying size

--- drawstuff.js
+++ drawstuff.js
@@ -80,7 +80,7 @@ function main() {
     // Draw a rectangle with pixels
     var c = new Color(255,0,0,255); // the color at the pixel: black opaque
     for (var x=50; x<100; x++) 
-        for (var y=50; y<100; y++) {
+        for (var y=50; y<800; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
