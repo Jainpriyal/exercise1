c83e23efc7c131a01ed308d30f2c55f93229b057
Jainpriyal <jainpriyal168@gmail.com>
2017-08-22 12:57:02 -0700

    modifying size and position

--- drawstuff.js
+++ drawstuff.js
@@ -80,10 +80,10 @@ function main() {
     // Draw a rectangle with pixels
     var c = new Color(255,0,0,255); // the color at the pixel: black opaque
     for (var x=50; x<100; x++) 
-        for (var y=50; y<75; y++) {
+        for (var y=50; y<100; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
     
-    context.putImageData(imagedata, 0, 0); // display the image in the context
+    context.putImageData(imagedata, 120, 0); // display the image in the context
 }
